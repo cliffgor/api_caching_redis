dfba75def540b99f7c80c02c3b8d2b2db3fdfd68
clffgor <clierozigy@gmail.com>
2022-08-11 18:52:24 +0300

    Added redis io and started cahing the data from the API reducing the response time to become very first

--- api.js
+++ api.js
@@ -1,11 +1,31 @@
 import axios from 'axios'
+import Redis from 'ioredis'
 import "dotenv/config"
 
+const redis = new Redis({
+    'port':6379,
+    'host': '127.0.0.1'
+})
 const cityEndpoint = (city) => `http://api.openweathermap.org/data/2.5/weather?q=${city}&unitsmetric&appid=${process.env.WEATHER_API_KEY}`
 
 const getWeather = async (city) => {
-    let apiResponse = await axios.get(cityEndpoint(city))
 
+    // Here we check if we have cached value of the city weather we want
+    let cacheEntry = await redis.get(`weather:${city}`)
+    // If we have a cache hit
+    if (cacheEntry) {
+        cacheEntry =  JSON.parse(cacheEntry)
+          // Rerurn entry 
+
+          return {...cacheEntry, 'source' : 'Cache'}
+    }
+
+    // we must have a cache miss
+  
+
+    // Othewise Here we are calling the API response
+    let apiResponse = await axios.get(cityEndpoint(city))
+    redis.set(`weather:${city}`, JSON.stringify(apiResponse.data))
     return {...apiResponse.data, 'source': 'API'}
 }
 
